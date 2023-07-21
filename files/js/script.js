0f2c17da8592405bc5c02bda9ada7ae0e00a2d79
Vyacheslav <mister.slaus@gmail.com>
2023-07-21 18:52:51 +0300

    Add files via upload

--- files/js/script.js
+++ files/js/script.js
@@ -1,53 +1,363 @@
 window.addEventListener('DOMContentLoaded', function (event) {
     'use strict';
 
-    const flavoursButton = document.querySelectorAll('.flavours-name');
-
-    const closeDescription = () => {
-        flavoursButton.forEach((item) => {
-            item.closest('.flavours-btn').classList.remove('active');
-        });
+    const isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows()
+            );
+        },
     }
 
-    flavoursButton.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-            closeDescription();
+    if (isMobile.any()) {
+        this.document.body.classList.add('touch');
+    } else {
+        this.document.body.classList.add('desktop');
+    }
 
-            item.closest('.flavours-btn').classList.add('active');
-        });
+
+    var splide = new Splide('.splide', {
+        updateOnMove: true,
+        type: 'loop',
+        perPage: 4,
+        perMove: 1,
+        interval: 7000,
+        breakpoints: {
+            1199: {
+                perPage: 3,
+            },
+            991: {
+                perPage: 2,
+            },
+            767: {
+                perPage: 1,
+            }
+        },
+        autoplay: true,
+        arrows: false,
+        pagination: false,
+        drag: true,
+        // wheel: true,
+        // direction: 'ttb'
     });
 
-    const closeFlavoursButton = document.querySelectorAll('.flavours-close');
+    splide.mount();
 
-    closeFlavoursButton.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-            closeDescription();
-        });
+
+
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    // if (ScrollTrigger.isTouch !== 1) {
+
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.7,
+        effects: true
     });
+
+    let prevScrollY = 0;
+    const tagBody = document.body;
+
+    gsap.fromTo('#header', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '#header',
+            start: '60%',
+            end: '120%',
+            scrub: true,
+            onUpdate: (self) => {
+                const scrollY = self.progress;
+                if (scrollY < prevScrollY) {
+                    tagBody.style.backgroundColor = '#d8f0f2';
+                } else {
+                    tagBody.style.backgroundColor = '#d8f0f2';
+                }
+                prevScrollY = scrollY;
+            }
+        }
+    });
+
+    gsap.fromTo('#story', { 
+        opacity: 1 
+    }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '#story',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    });
+
+    let storyItemsLeft = gsap.utils.toArray('.story .story-block-text')
+
+    storyItemsLeft.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: -150
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-650',
+                end: '-200',
+                scrub: true
+            }
+        })
+    })
+
+    let storyItemsRight = gsap.utils.toArray('.story .story-block-image')
+
+    storyItemsRight.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            width: '150%',
+            x: 200,
+            left: '300%',
+        }, {
+            opacity: 1,
+            width: '100%',
+            x: 0,
+            left: '100%',
+            scrollTrigger: {
+                trigger: item,
+                start: '-800',
+                end: '-200',
+                scrub: true
+            }
+        })
+    })
+
+    gsap.fromTo('.story-certificate', { 
+        height: '120px', 
+        opacity: 1, 
+        bottom: '0px' 
+    }, {
+        height: '0px',
+        bottom: '120px',
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.story-certificate',
+            start: '-300%',
+            end: '-40%',
+            scrub: true,
+        }
+    });
+
+    // gsap.fromTo('#flavours', { 
+    //     opacity: 1 
+    // }, {
+    //     opacity: 0,
+    //     scrollTrigger: {
+    //         trigger: '#flavours',
+    //         start: 'bottom',
+    //         end: '110%',
+    //         scrub: true,
+    //         onUpdate: (self) => {
+    //             const scrollY = self.progress;
+    //             if (scrollY < prevScrollY) {
+    //                 tagBody.style.backgroundColor = '#FEDDEE';
+    //             } else {
+    //                 tagBody.style.backgroundColor = '#FEDDEE';
+    //             }
+    //             prevScrollY = scrollY;
+    //         }
+    //     }
+    // });
+
+    let flavoursItemsFirst = gsap.utils.toArray('.flavours .flavours-item:nth-child(3n+1)')
+
+    flavoursItemsFirst.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: -100,
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-700',
+                end: '-350',
+                scrub: true
+            }
+        })
+    })
+
+    let flavoursItemsSecond = gsap.utils.toArray('.flavours .flavours-item:nth-child(3n+2)')
+
+    flavoursItemsSecond.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: 100,
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-750',
+                end: '-400',
+                scrub: true
+            }
+        })
+    })
+
+    let flavoursItemsThird = gsap.utils.toArray('.flavours .flavours-item:nth-child(3n+3)')
+
+    flavoursItemsThird.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: 100,
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-500',
+                end: '-300',
+                scrub: true
+            }
+        })
+    })
+
+    
+
+    gsap.fromTo('#whosale', { 
+        opacity: 1 
+    }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '#whosale',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    });
+
+    let whosaleItemsLeft = gsap.utils.toArray('.whosale .whosale-block-text')
+
+    whosaleItemsLeft.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: -50
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-650',
+                end: '-100',
+                scrub: true
+            }
+        })
+    })
+
+    let whosaleItemsRight = gsap.utils.toArray('.whosale .whosale-block-image')
+
+    whosaleItemsRight.forEach(item => {
+        gsap.fromTo(item, {
+            opacity: 0,
+            x: 200
+        }, {
+            opacity: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                start: '-800',
+                end: '-200',
+                scrub: true
+            }
+        })
+    })
+
+    gsap.fromTo('#flavours', { 
+        opacity: 1 
+    }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '#flavours',
+            start: 'center',
+            end: 'bottom',
+            scrub: true,
+            onUpdate: (self) => {
+                const scrollY = self.progress;
+                if (scrollY < prevScrollY) {
+                    tagBody.style.backgroundColor = '#FEDDEE';
+                } else {
+                    tagBody.style.backgroundColor = '#FEDDEE';
+                }
+                prevScrollY = scrollY;
+            }
+        }
+    });
+
+
+    gsap.fromTo('#contact', { 
+        backgroundPosition: 'center 150%',
+    }, {
+        backgroundPosition: 'center 100%',
+        scrollTrigger: {
+            trigger: '#contact',
+            start: '-300',
+            end: '100',
+            scrub: true
+        }
+    });
+
+    // }
+
+
+
+
+
+
+
+    
 
     var sections = document.querySelectorAll('section');
     var navLinks = document.querySelectorAll('.menu-block .menu-block-link');
 
     window.addEventListener('scroll', function () {
         var currentSection = '';
-
+        var scrollPosition = window.pageYOffset;
+        var windowHeight = window.innerHeight;
+      
         sections.forEach(function (section) {
-            var sectionTop = section.offsetTop;
-            var sectionHeight = section.offsetHeight;
-            if (window.pageYOffset >= sectionTop - sectionHeight / 2) {
-                currentSection = section.getAttribute('id');
-            }
+          var sectionTop = section.offsetTop;
+          var sectionHeight = section.offsetHeight;
+          
+          if (scrollPosition >= sectionTop - windowHeight / 2) {
+            currentSection = section.getAttribute('id');
+          }
         });
-
+      
         navLinks.forEach(function (link) {
-            link.classList.remove('active');
-            if (link.getAttribute('href').substring(1) === currentSection) {
-                link.classList.add('active');
-            }
+          link.classList.remove('active');
+          if (link.getAttribute('href').substring(1) === currentSection) {
+            link.classList.add('active');
+          }
         });
-    });
+      });
 
     navLinks.forEach(function (link) {
         link.addEventListener('click', function (e) {
